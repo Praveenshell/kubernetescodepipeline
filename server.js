05e87de8d21fc03167ccf1cd4fe498066602b6e0
Praveenshell <praveenshell@gmail.com>
2022-02-25 17:29:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Container Hardening New Offering from TCS');
+  res.send('Container Hardening New Offering from TCS - Thanks Thiyagu');
 });
 
 app.listen(PORT, HOST);
