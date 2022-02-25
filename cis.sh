2967909d4443c8549e72f082fd5951e6565161dd
Praveenshell <praveenshell@gmail.com>
2022-02-25 10:07:20 +0530

    Create cis.sh

---
+++ cis.sh
@@ -0,0 +1,13 @@
+# Ubuntu Servers
+USERNAME=noroot
+USER_UID=8001
+USER_GID=$USER_UID
+
+
+# Create the user
+groupadd --gid $USER_GID $USERNAME \
+&& useradd --uid $USER_UID --gid $USER_GID -m $USERNAME \
+&& apt-get update \
+&& apt-get install -y sudo \
+&& echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
+&& chmod 0440 /etc/sudoers.d/$USERNAME
