a050de14211a9968fc006945f59e5f732891b73a
Praveenshell <praveenshell@gmail.com>
2022-02-25 12:08:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Forum 2022');
+  res.send('Hello World Forum 2022 blaster');
 });
 
 app.listen(PORT, HOST);
