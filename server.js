b5d77f6b7078c568ed5e2abcc938349385795aac
Praveenshell <praveenshell@gmail.com>
2022-03-06 14:43:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('TCS DEMO - Container Hardening and Zero touch deployment %date');
-   var datetime = new Date();
+  res.send('TCS DEMO - Container Hardening and Zero touch deployment);
 });
 
 app.listen(PORT, HOST);
