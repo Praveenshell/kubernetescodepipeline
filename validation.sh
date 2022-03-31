0a06a30b38bcbc3d4a8d11e800ae0c76e0b8bd99
Praveenshell <praveenshell@gmail.com>
2022-03-31 10:26:12 +0530

    Validation Script

---
+++ validation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker run -d nginx:latest
+
+cd /data/docker-bench-security/
+
+bash docker-bench-security.sh > /tmp/out.txt
+
+a=`cat /tmp/out.txt | grep -i Score: | awk {'print $3'}`
+echo "$a"
+if [ $a -gt 10 ];
+then
+        echo "success"
+else
+        echo "failure"
+fi
