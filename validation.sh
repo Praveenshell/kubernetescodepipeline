8922261f41ffcb52fac375cfe5ced1a3ab110f07
Praveenshell <praveenshell@gmail.com>
2022-04-01 12:46:45 +0530

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -3,7 +3,7 @@ docker run -d nginx:latest sleep 30
 
 cd /data/docker-bench-security/
 
-bash docker-bench-security.sh > /tmp/out.txt
+bash docker-bench-security.sh > /mnt/out.txt
 
 a=`cat /tmp/out.txt | grep -i Score: | awk {'print $3'}`
 echo "$a"
