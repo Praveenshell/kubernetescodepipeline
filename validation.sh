7bdbbe2ad6eef7975e81e323a36c636d76ab4ba2
Praveenshell <praveenshell@gmail.com>
2022-04-01 12:55:33 +0530

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -4,10 +4,10 @@ docker run -d nginx:latest sleep 30
 cd /data/docker-bench-security/
 
 bash docker-bench-security.sh > /mnt/out.txt
-
+chmod +x /mnt/out.txt
 a=`cat /tmp/out.txt | grep -i Score: | awk {'print $3'}`
 echo "$a"
-if [ $a -gt 10 ];
+if [ $a -gt 5 ];
 then
         echo "image_success"
 else
