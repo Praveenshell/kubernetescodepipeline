19f59949f7b6d496f6d938be966a7a7580056d20
Praveenshell <praveenshell@gmail.com>
2022-04-05 12:15:11 +0530

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -3,9 +3,9 @@ docker run -d nginx:latest sleep 30 > /dev/null 2>&1
 
 unzip docker-bench-security-master.zip
 cd docker-bench-security-master
-bash docker-bench-security.sh | tee stdout.txt
-chmod +x stdout.txt
-a=`cat stdout.txt | grep -i Score: | awk {'print $3'}`
+bash docker-bench-security.sh | tee /jobsdata/stdout.txt
+chmod +x /jobsdata/stdout.txt
+a=`cat /jobsdata/stdout.txt | grep -i Score: | awk {'print $3'}`
 echo "$a"
 if [ $a -gt 5 ];
 then
@@ -13,5 +13,5 @@ then
 else
         echo "image_failure"
 fi
-cp -f stdout.txt stdout-`date`.txt
-rm -f stdout.txt
+cp -f /jobsdata/stdout.txt /jobsdata/stdout-`date`.txt
+rm -f /jobsdata/stdout.txt
