1fb88adaa1caecee5762b584bb25ed8d55947242
Praveenshell <praveenshell@gmail.com>
2022-04-05 10:40:21 +0530

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -3,9 +3,9 @@ docker run -d nginx:latest sleep 30
 
 cd /data/docker-bench-security/
 
-bash docker-bench-security.sh > /mnt/out.txt
+bash docker-bench-security.sh > stdout.txt
 chmod +x /mnt/out.txt
-a=`cat /tmp/out.txt | grep -i Score: | awk {'print $3'}`
+a=`cat stdout.txt | grep -i Score: | awk {'print $3'}`
 echo "$a"
 if [ $a -gt 5 ];
 then
