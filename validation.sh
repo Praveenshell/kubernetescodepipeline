2818347f92c41ae45af9a5c298123341bfcf103a
Praveenshell <praveenshell@gmail.com>
2022-04-05 14:03:13 +0530

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -4,8 +4,8 @@ sudo rm -rf docker-bench-security
 
 unzip docker-bench-security.zip > /dev/null 3>&1
 cd docker-bench-security
-chown -R root:root .
-chmod +x docker-bench-security.sh
+
+sudo chmod +x docker-bench-security.sh
 sudo bash docker-bench-security.sh > /jobsdata/stdout.txt
 chmod +x /jobsdata/stdout.txt
 a=`cat /jobsdata/stdout.txt | grep -i Score: | awk {'print $3'}`
